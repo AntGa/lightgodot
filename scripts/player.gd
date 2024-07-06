11498f41ec6d912ef3a48f84bdd3218739417fc0
Anton Garay <antonvince.garay@gmail.com>
2024-07-06 22:56:27 +1200

    Added camera movement via mouse, a basic background and tested exporting.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,27 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
-	# Add the gravity.
-	
-		
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	velocity.x = direction.x * SPEED
+	velocity.y = direction.y * SPEED
+	velocity = velocity.move_toward(Vector2.ZERO, SPEED * delta)
 	move_and_slide()
+
+func _process(delta):
+	# Update camera position based on mouse position
+	var mouse_offset = (get_viewport().get_mouse_position() - get_viewport().size / 2.0)
+	$Camera2D.position = lerp(Vector2(), mouse_offset.normalized() * 500, mouse_offset.length() / 1000)
