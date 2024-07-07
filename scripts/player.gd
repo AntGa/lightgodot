1f6cc250e67993fb7c857f830e83cad9167ae34f
Tulza <40633228+tulza@users.noreply.github.com>
2024-07-07 18:31:39 +1200

    Add limit to camera movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED: float = 300.0
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	velocity.x = direction.x * SPEED
 	velocity.y = direction.y * SPEED
 	velocity = velocity.move_toward(Vector2.ZERO, SPEED * delta)
 	move_and_slide()
 
-func _process(delta):
+func _process(delta) -> void:
 	# Update camera position based on mouse position
 	var mouse_offset = (get_viewport().get_mouse_position() - get_viewport().size / 2.0)
 	$Camera2D.position = lerp(Vector2(), mouse_offset.normalized() * 500, mouse_offset.length() / 1000)
