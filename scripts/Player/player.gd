8ecf492f1d494224b89905e9d082306ed78339f3
Anton Garay <antonvince.garay@gmail.com>
2024-07-09 23:40:15 +1200

    fixed names

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -2,7 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 const SPEED: float = 300.0
-var bullet = preload("res://scenes/bullet.tscn")
+var bullet_sprite = preload("res://scenes/bullet.tscn")
 var can_shoot: bool = true
 var aim_position : Vector2 = Vector2(1, 0)
 
@@ -15,7 +15,7 @@ func _exit_tree() -> void:
 func _physics_process(delta) -> void:
 	if Input.is_action_just_pressed("click") and Global.node_creation_parent != null:
 		$reloadspeed.start()
-		Global.instance_node(bullet, global_position, Global.node_creation_parent)
+		Global.instance_node(bullet_sprite, global_position, Global.node_creation_parent)
 		can_shoot = false
 
 func _on_reload_speed_timeout() -> void:
